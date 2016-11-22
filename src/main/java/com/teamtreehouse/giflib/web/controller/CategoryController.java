de05b4a4709d18ec1b43b24ae8725927289c1b5c
Mallikarjuna <drop2ms@gmail.com>
2016-11-22 10:18:43 +0530

    Data Validation : Included Validation Message to Valid Category Name and its Color Code

--- src/main/java/com/teamtreehouse/giflib/web/controller/CategoryController.java
+++ src/main/java/com/teamtreehouse/giflib/web/controller/CategoryController.java
@@ -9,10 +9,13 @@ import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import javax.validation.Valid;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -45,7 +48,9 @@ public class CategoryController {
     @RequestMapping("categories/add")
     public String formNewCategory(Model model) {
         // TODO: Add model attributes needed for new form
-        model.addAttribute("category",new Category());
+        if (!model.containsAttribute("category")) {
+            model.addAttribute("category", new Category());
+        }
         model.addAttribute("colors", Color.values());
         return "category/form";
     }
@@ -69,8 +74,13 @@ public class CategoryController {
 
     // Add a category
     @RequestMapping(value = "/categories", method = RequestMethod.POST)
-    public String addCategory(Category category) {
+    public String addCategory(@Valid Category category, BindingResult result, RedirectAttributes redirectAttributes) {
         // TODO: Add category if valid data was received
+        if (result.hasErrors()) {
+            redirectAttributes.addFlashAttribute("category",category);
+            redirectAttributes.addFlashAttribute("org.springframework.validation.BindingResult.category",result);
+            return "redirect:/categories/add";
+        }
             categoryService.add(category);
         // TODO: Redirect browser to /categories
         return "redirect:/categories";
